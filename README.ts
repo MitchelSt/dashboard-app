4d1cbb40b0d28d6b736009b52a5ac4a92c97b872
mitchelst <mitchel.stroosnijder@gmail.com>
2021-08-02 01:04:44 +0200

    add: snippets

---
+++ README.ts
@@ -0,0 +1,80 @@
+const todayExpenses = [
+  {
+    id: 1,
+    expense: "Grocery",
+    time: "5:12 pm",
+    location: "Belanja di pascar",
+    price: 326.8,
+    icon: cartIcon,
+    iconBackgroundColor: "#32a7e2",
+  },
+  {
+    id: 2,
+    expense: "Transportation",
+    time: "5:12 pm",
+    location: "Naik bus umum",
+    price: 15.0,
+    icon: transportIcon,
+    iconBackgroundColor: "#B548C6",
+  },
+  {
+    id: 3,
+    expense: "Housing",
+    time: "5:12 pm",
+    location: "Bayar Listrik",
+    price: 185.75,
+    icon: houseIcon,
+    iconBackgroundColor: "#FF8700",
+  },
+];
+
+
+const previousExpenses = [
+  {
+    id: 1,
+    expense: "Food and Drink",
+    time: "5:12 pm",
+    location: "Makan Steak",
+    price: 156.0,
+    icon: cartIcon,
+    iconBackgroundColor: "#DC3434",
+  },
+  {
+    id: 2,
+    expense: "Entertainment",
+    time: "5:12 pm",
+    location: "Nonton Bioskop",
+    price: 35.2,
+    icon: transportIcon,
+    iconBackgroundColor: "#4BA83D",
+  },
+];
+
+
+const spendCategories = [
+  {
+    id: 1,
+    category: "Food and Drinks",
+    price: 872.4,
+  },
+  {
+    id: 2,
+    category: "Shopping",
+    price: 1378.2,
+  },
+  {
+    id: 3,
+    category: "Housing",
+    price: 928.5,
+  },
+  {
+    id: 4,
+    category: "Transportation",
+    price: 420.7,
+  },
+  {
+    id: 5,
+    category: "Vehicle",
+    price: 520,
+  },
+];
